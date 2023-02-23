01a61f338bdd8da1aef0d1a311446382d23e5db3
Trung Nguyễn Quốc <trungnguyenquoc@Trungs-MacBook-Pro.local>
2023-02-23 21:51:53 +0700

    Build Contact list in web

--- lib/layouts/web_screen_layout.dart
+++ lib/layouts/web_screen_layout.dart
@@ -1,13 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chatapp/widgets/contact_list.dart';
 
 class WebScreenLayout extends StatelessWidget {
   const WebScreenLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('This is Web Screen'),
+    return Scaffold(
+      body: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: const [
+                  ContactsList(),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.7,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/backgroundImage.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
