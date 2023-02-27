a084c6797e0dd3dd337a6898524975f1d595638e
Trung Nguyễn Quốc <trungnguyenquoc@Trungs-MacBook-Pro.local>
2023-02-27 10:30:34 +0700

    Build web profile bar

--- lib/layouts/web_screen_layout.dart
+++ lib/layouts/web_screen_layout.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_chatapp/widgets/contact_list.dart';
+import 'package:flutter_chatapp/widgets/web_profile_bar.dart';
 
 class WebScreenLayout extends StatelessWidget {
   const WebScreenLayout({super.key});
@@ -14,13 +15,14 @@ class WebScreenLayout extends StatelessWidget {
             child: SingleChildScrollView(
               child: Column(
                 children: const [
+                  WebProfileBar(),
                   ContactsList(),
                 ],
               ),
             ),
           ),
           Container(
-            width: MediaQuery.of(context).size.width * 0.7,
+            width: MediaQuery.of(context).size.width * 0.75,
             decoration: const BoxDecoration(
               image: DecorationImage(
                 image: AssetImage('assets/backgroundImage.png'),
