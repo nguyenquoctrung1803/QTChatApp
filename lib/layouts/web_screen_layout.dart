968231836654dd83900946a0f27c62103a1dcbc5
Trung Nguyễn Quốc <trungnguyenquoc@Trungs-MacBook-Pro.local>
2023-03-01 10:02:06 +0700

    build input text in web screen

--- lib/layouts/web_screen_layout.dart
+++ lib/layouts/web_screen_layout.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chatapp/utils/colors.dart';
 import 'package:flutter_chatapp/widgets/chat_list.dart';
 import 'package:flutter_chatapp/widgets/contact_list.dart';
 import 'package:flutter_chatapp/widgets/web_chat_appbar.dart';
@@ -35,8 +36,70 @@ class WebScreenLayout extends StatelessWidget {
             ),
             child: Column(
               children: [
-                WebChatAppBar(),
-                Expanded(child: ChatList()),
+                const WebChatAppBar(),
+                const Expanded(
+                  child: ChatList(),
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.07,
+                  padding: const EdgeInsets.all(10),
+                  decoration: const BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(
+                        color: dividerColor,
+                      ),
+                    ),
+                    color: chatBarMessage,
+                  ),
+                  child: Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.emoji_emotions_outlined,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.attach_file,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                            left: 10,
+                            right: 15,
+                          ),
+                          child: TextField(
+                            decoration: InputDecoration(
+                              fillColor: searchBarColor,
+                              filled: true,
+                              hintText: 'Type a messages',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20),
+                                borderSide: const BorderSide(
+                                  width: 0,
+                                  style: BorderStyle.none,
+                                ),
+                              ),
+                              contentPadding: const EdgeInsets.only(left: 20),
+                            ),
+                          ),
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.mic,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
               ],
             ),
           ),
